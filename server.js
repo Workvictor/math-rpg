a2a0ee157f5069473ad6b5de2df67f22be10e01b
Viktor Punko <victorpunko@gmail.com>
2019-11-24 20:33:32 +0300

    Add app compression

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const favicon = require('express-favicon');
+const compression = require('compression');
 const path = require('path');
 const port = process.env.PORT || 8080;
 const app = express();
+app.use(compression());
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
